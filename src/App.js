033548c1c4263325cf90018bfb4074b9969fea80
shengyunn <ryan082795@gmail.com>
2024-05-29 09:53:21 +0800

    Yun

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyRGBPanel from "./components/MyRGBPanel";
+import MySlider from "./components/MySlider";
+import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <div className="box">
+        <h1>MySlider</h1>
+        <MySlider />
+      </div>
+
+      <div className="box">
+        <h1>MyRGBPanel</h1>
+        <MyRGBPanel />
+      </div>
+      
+      <div className="box">
+        <h1>MyCalculator</h1>
+        <MyCalculator />
+      
+      </div>
     </div>
   );
 }
