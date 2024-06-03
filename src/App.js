62cbe18fa2df40e71b790c1df3f65b92331f1a3f
shengyunn <ryan082795@gmail.com>
2024-06-03 19:01:24 +0800

    Slider

--- src/App.js
+++ src/App.js
@@ -5,22 +5,19 @@ import MyCalculator from "./components/MyCalculator";
 
 function App() {
   return (
+    
     <div className="App">
-      
+      <div className="title">
+        <p>React  Element</p>
+      </div>
+      <div className="big-box">
       <div className="box">
         <h1>MySlider</h1>
         <MySlider />
       </div>
 
-      <div className="box">
-        <h1>MyRGBPanel</h1>
-        <MyRGBPanel />
-      </div>
       
-      <div className="box">
-        <h1>MyCalculator</h1>
-        <MyCalculator />
-      
+
       </div>
     </div>
   );
