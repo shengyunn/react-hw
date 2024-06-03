27100b6f64e63eb437a431d0d9f5856c8e642ddb
shengyunn <ryan082795@gmail.com>
2024-06-03 19:21:34 +0800

    Yun

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
-import MyRGBPanel from "./components/MyRGBPanel";
-import MySlider from "./components/MySlider";
-import MyCalculator from "./components/MyCalculator";
+import MyRGBPanel from "./components/rgb-panel/MyRGBPanel";
+import MySlider from "./components/rgb-panel/MySlider";
+import MyCalculator from "./components/calculator/MyCalculator";
+import TicTacToe from "./components/tic-tac-toe/TicTacToe";
 
 function App() {
   return (
@@ -24,6 +25,10 @@ function App() {
         <div className="box">
           <h1>MyCalculator</h1>
           <MyCalculator /> 
+        </div>
+        <div className="box">
+          <h1>Tic-Tac-Toe Game</h1>
+          <TicTacToe /> 
         </div>
       </div>
     </div>
